da5e6eecd612fe0ca1b37a626925966a9aba62dd
Knealand <knealandy@gmail.com>
2020-03-18 00:26:55 -0400

    test 7

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addCollection("doc", (collection) =>
-        collection.getFilteredByGlob("documentation/*.md").sort((a, b) => {
-            return b.position - a.position;
-        })
-    );
+    // eleventyConfig.addCollection("doc", (collection) =>
+    //     collection.getFilteredByGlob("documentation/*.md").sort((a, b) => {
+    //         return b.position - a.position;
+    //     })
+    // );
+
+    eleventyConfig.addCollection("doc", function(collection) {
+        return collection.documentation.sort(function(a, b) {
+          return b.date - a.date;
+        });
+      });
 
 
     eleventyConfig.addPassthroughCopy('Images');
