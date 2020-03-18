78e69590ec19fc47fc784e345d151e6011fb2859
Knealand <knealandy@gmail.com>
2020-03-17 23:33:59 -0400

    big push 1

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addCollection("documentation", function(collection) {
+        return collection.getAll().sort(function(a, b) {
+          return b.position - a.position;
+        });
+      });
+
     eleventyConfig.addPassthroughCopy('Images');
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('js');
