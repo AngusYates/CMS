5084b46e6431c5028a806188d6998ff5cc0c0006
Knealand <knealandy@gmail.com>
2020-03-18 00:49:00 -0400

    test 11

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
         var temp = collection.getAll();
         var temp2 = [];
         for(page in temp){
-            if(page.tags == "documentation"){
+            if(page.tags.includes("documentation")){
                 temp2.push(page);
             }
         }
